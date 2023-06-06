cf3a69d9075755b5bee3ff9449d4534405ccf908
42_Nige <42_Legin@MacBook-Pro-de-Nige-Paris.local>
2023-06-06 08:58:59 +0200

    Makefile updated added tidy clean up *.o

--- Makefile
+++ Makefile
@@ -1,22 +1,37 @@
 EXEC=armstrong_numbers
-CFLAGS=-Wall -Wextra -Werror
+CFLAGS=-Wall
+CFLAGS+=-Wextra
+CFLAGS+=-Werror
 CC=gcc
-FUNCTION=armstrong_numbers
+SOURCE_FILE=armstrong_numbers
 
 .PHONY: run
 run: $(EXEC)
+	@echo "\nRunning...file : $(EXEC)"
 	./$(EXEC)
+	@echo "\n"
 
-$(EXEC): main.o $(FUNCTION).o
+$(EXEC): main.o $(SOURCE_FILE).o
+	@echo "\nLinking.............."
 	$(CC) $^ -o $(EXEC)
 
-main.o: main.c $(FUNCTION).h
+main.o: main.c $(SOURCE_FILE).h
+	@echo "\nCompiling main.............."
 	$(CC) $(CFLAGS) -c main.c
 
-$(FUNCTION).o: $(FUNCTION).c
-	$(CC) $(CFLAGS) -c $(FUNCTION).c
+$(SOURCE_FILE).o: $(SOURCE_FILE).c
+	@echo "\nCompiling source file............."
+	$(CC) $(CFLAGS) -c $(SOURCE_FILE).c
 
+
+.PHONY: tidy
+tidy:
+	@echo "\nCleanng all .o files......."
+	rm -rf *.o
+	@echo "\n"
 
 .PHONY: clean
 clean:
+	@echo "\nClean up all ......."
 	rm -rf $(EXEC) *.o
+	@echo "\n"
--- armstrong_numbers.c
+++ armstrong_numbers.c
@@ -6,14 +6,13 @@
 /*   By: 42_Legin <Nige@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 20:46:33 by 42_Legin          #+#    #+#             */
-/*   Updated: 2023/06/05 22:41:17 by 42_Legin         ###   ########.fr       */
+/*   Updated: 2023/06/06 08:01:30 by 42_Legin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "armstrong_numbers.h"
 
 static int	ft_get_nbr_of_digits(int number);
-static int	ft_check_number_is_positive(int nbr);
 static int	ft_pow(int power, int current_digit);
 
 bool	is_armstrong_number(int candidate)
@@ -26,8 +25,9 @@ bool	is_armstrong_number(int candidate)
 
 	original_number = candidate;
 	sum = 0;
-	candidate = ft_check_number_is_positive(original_number);
 	power = ft_get_nbr_of_digits(original_number);
+	if(candidate < 0)
+		candidate = false;
 	while (candidate)
 	{
 		current_digit = candidate % 10;
@@ -44,7 +44,7 @@ bool	is_armstrong_number(int candidate)
 
 static int	ft_get_nbr_of_digits(int number)
 {
-	int	nbr_of_digits;
+		  int	nbr_of_digits;
 
 	nbr_of_digits = 0;
 	while (number)
@@ -56,28 +56,17 @@ static int	ft_get_nbr_of_digits(int number)
 }
 
 /**
-*	checks if number is positive and returns the number or 0 if negative
-*/
-
-static int	ft_check_number_is_positive(int nbr)
-{
-	if (nbr < 0)
-		nbr = 0;
-	return (nbr);
-}
-
-/**
 *	gets the sum of a power calculation 3^3 returns 27 
 */
 
-static int	ft_pow(int nbr_of_iterations, int current_digit)
+static int	ft_pow(int base, int power)
 
 {
 	int	iteration;
 	int	result = 1;
 
 	iteration = 0;
-	while ((iteration++) != nbr_of_iterations)
-		result = result * current_digit;
+	while ((iteration++) != base)
+		result = result * power;
 	return (result);
 }
